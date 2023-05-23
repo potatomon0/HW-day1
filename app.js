fa3f8a4249d6bae8c80e6ab18bff106f8228343e
Hu <huang.h0709@gmail.com>
2023-05-23 15:41:23 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*const button = document.querySelector("button");
+const age = document.querySelector(".age");
+console.log(button);
+button.addEventListener("click",function(){
+    age.innerHTML = "27";
+
+})*/
+function age(){
+    document.getElementById("myAge").innerHTML="27";
+}
